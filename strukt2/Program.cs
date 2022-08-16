4c6ca211200bd2bb6a6a454e5de695322c26b70b
EVGEN <mtmsys4@gmail.com>
2022-08-17 02:46:51 +0300

    ini git ignore

---
+++ strukt2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Привет я Андрей, и я ненавижу песни Аллегровой ");
+
+Console.Write("Введите Ваше имя: ");
+
+string username = Console.ReadLine();
+Console.Write("Привет" + " " + username + "! " );
+
+
+
+

65f7c5f2052d95dcdafa2ee4e3fcefdc58909bec
EVGEN <mtmsys4@gmail.com>
2022-08-17 04:25:58 +0300

    Добавили деление

--- agent004/Program.cs
+++ agent004/Program.cs
@@ -1,3 +1,18 @@
 ﻿
 
-Console.WriteLine("Hello, World!");
+Console.Write("Привет! введи делимое: ");
+
+double delimoe = 55;
+
+Console.Write("Пожалуйста, введите делитель: ");
+
+double delitel = 5;
+
+Console.Write("Ответ равен: ");
+
+ double chastnoe = delimoe / delitel;
+
+
+
+
+Console.Write((chastnoe) + " ");
